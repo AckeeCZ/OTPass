90d2c267debd7572d1326ddd5a4a1cc98ba5a2b9
ladislav.louka <ladislav.louka@ackee.cz>
2022-04-29 14:44:27 +0200

    TOTP generation

---
+++ src/lib/totp.ts
@@ -0,0 +1,25 @@
+import { HOTP } from 'index'
+import { HMACAlgorithm } from './utils'
+
+export const generate = (
+  secret: Buffer,
+  options: {
+    codeLength?: number
+    truncationOffset?: number
+    algorithm?: HMACAlgorithm
+    receiveTime?: number
+    timeStep?: number
+    clockStart?: number
+  } = {}
+) => {
+  const {
+    receiveTime = Math.floor(new Date().getTime() / 1000),
+    timeStep = 30,
+    clockStart = 0,
+  } = options
+  return HOTP.generate(
+    secret,
+    Math.floor((receiveTime - clockStart) / timeStep),
+    options
+  )
+}
